34282a62cc3a37efbc057594402f6f70799adb45
Pete Godston <56722508+satch421@users.noreply.github.com>
2021-08-29 06:54:00 -0400

    Beta Device Mgmt
    This is beta python code to manage the data transfer from an obsolete device to a snow device in preparation for removing the obsolete device from the network and accessing data (uploaded to a snow device) still required via an M&PI account.

---
+++ Ice_Store.py
@@ -0,0 +1,37 @@
+# ewise_sales = pgodston@ewiseinc.com
+
+print("We are saving  all data more recent than <timestamp>?", end='  ')
+savestamp = input()
+print("What is the IP address for this source?", end='  ')
+
+sourceIP = input()
+print("What is the access password for this source?", end='  ')
+sourcePW = input()
+
+print(f"Connecting Snow device and {sourceIP} ...!")
+print(f"Searching for data more recent than {savestamp} ...!")
+
+print(f"Transfering data more recent than {savestamp} from {sourceIP} to Snow device ...!")
+
+
+print("Data transfer complete.")
+print(f"Terminating connection with {sourceIP}")
+
+
+
+print(f"Who is the administrator to handle the data from {sourceIP}?", end='  ')
+dataAdmin = input()
+print(f"{dataAdmin} works in which department?", end='  ')
+adminDept = input()
+
+print(f"What is {dataAdmin}'s phone?", end='  ')
+dataAdmin_phone = input()
+print(f"What is {dataAdmin}'s email address?", end='  ')
+dataAdmin_email = input()
+
+print(f"We have recorded this information in our Snow device database for {sourceIP}.")
+
+# send this email to {dataAdmin_email}
+
+
+print (f"Data for your {dataAdmin} patients has been saved to Deep Storage.")
